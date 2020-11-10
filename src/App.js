f3bcfb7dad0da6a8ab1d043686a3a0d5a26db173
Mohit Kalyan <mohit.kalyan@gmail.com>
2020-11-10 21:11:37 +0530

    app complete with navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const list = [
   {
@@ -16,16 +18,34 @@ const list = [
   { title: "How to lear Redux", description: "Just by doing and programming" },
 ];
 
+const options = [
+  { label: "Color Red", value: "red" },
+  { label: "Color Green", value: "green" },
+  { label: "Always Blue", value: "blue" },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      {
-        //<Accordion list={list} />
-      }
-      {
-        //<Search />
-      }
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion list={list} />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          options={options}
+          selected={selected}
+          onSelectChange={setSelected}
+        />
+      </Route>
     </div>
   );
 };
